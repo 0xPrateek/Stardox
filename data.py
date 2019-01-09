d90f514b7d875823386ab3d0b0217583fa7369ee
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-01-09 22:50:01 +0530

    Intermediate variables for ploting the tree

---
+++ data.py
@@ -0,0 +1,20 @@
+#For Storing name of the stargazers
+name_list=[]
+
+#For Storing Username of stargazers
+username_list=[]
+
+#For Storing Total stars by the stargazers
+star_list=[]
+
+#For Storing Total number of repositories by stargazers
+repo_list=[]
+
+#For Storing Total number of followers of stargazers
+followers_list=[]
+
+#For Storing Total number of following of stargazers
+following_list=[]
+
+#For Storing the Repository name
+header=""
