e4ff4f8b13cb725dfb18394e38ce9ef979f97be2
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-01-12 19:34:05 +0530

    Setup for Stardox

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+   name='Stardox',
+   version='1.0.0',
+   description="Github Stargazers information gathering tool",
+   long_description=long_description,
+   url='https://github.com/0xprateek/stardox',
+   packages=['Stardox'],
+   install_requires=requirements
+)
