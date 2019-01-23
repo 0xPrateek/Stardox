c2f666581a5f4f5b955d9670273260b1382c13f8
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-01-23 10:03:56 -0800

    Adding List for storing emails.

--- src/data.py
+++ src/data.py
@@ -13,6 +13,9 @@ repo_list=[]
 #For Storing Total number of followers of stargazers
 followers_list=[]
 
+#For Storing emails of all the stargazers
+email_list=[]
+
 #For Storing Total number of following of stargazers
 following_list=[]
 
