95b0a978abb41052ce0c5355ae885e7eba06520e
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-02-02 08:33:11 -0800

    Adding Logo.py to Logo directory

---
+++ Logo/logo.py
@@ -0,0 +1,13 @@
+def header():
+    print('''
+\033[93m           ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+\033[93m          sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+\033[93m         sssssss        \033[31m    ssssssssss      ss     ss       sss     ss      sss    ss       ss      ss   sss    sss
+\033[93m        sssssss         \033[31m      ssss        sss     sss      sss      ss     sss      ss     ss      ss     ss   ss
+\033[93m       ssssssssssssss   \033[31m     ssss        sss     sss      sss      ss     sss       ss    ss      ss      ss  ss
+\033[93m       ssssssssssssss   \033[31m    ssss        sssssssssss      sssssssssss     sss       ss    ss      ss       ssss
+\033[93m                ssss    \033[31m   ssss        sssssssssss      sssssss         sss       ss    ss      ss       ssss
+\033[93m               ssss     \033[31m  ssss        sss     sss      sss  sss        sss       ss    ss      ss       ss  ss
+\033[93m    ssssssssssssss      \033[31m ssss        sss     sss      sss   sss       sss     ss     ssssssssss        ss   ss
+\033[93m  sssssssssssssss       \033[31mssss        sss     sss      sss     sss     sssssssss      ssssssssss       sss    sss       \033[137m Made By :\033[93m Pr0t0n
+    ''')
