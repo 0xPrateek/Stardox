2e2099253f02082164973e47cdb88b130b0f28ae
Prateek Mishra <41592999+0xPrateek@users.noreply.github.com>
2019-05-31 11:32:04 +0530

    Improvements for the loading speed.

--- src/stardox.py
+++ src/stardox.py
@@ -1,15 +1,10 @@
-import requests
-import structer
-import data
 import sys
 import colors
 import Logo
 import time
-from bs4 import BeautifulSoup
 import argparse
 
 def formated(string):
-
     new_string=[]
     string=list(string)
     for value in string:
@@ -18,17 +13,13 @@ def formated(string):
     string=''.join(new_string)
     return string
 
-
 def getting_header(soup_text):
-
     title=soup_text.title.get_text()
     start=title.find('/')
     stop=title.find(':')
     return title[start+1:stop]
 
-
 def format_url(url):
-
     if url.startswith('http://'):
         url=url.replace('http','https')
     elif url.startswith('www.'):
@@ -40,9 +31,7 @@ def format_url(url):
         sys.exit(1)
     return url
 
-
 def verify_url(page_data):
-
     data=str(page_data)
     if "Popular repositories" in data:
         return False
@@ -51,18 +40,13 @@ def verify_url(page_data):
     else:
         return True
 
-
 def get_latest_commit(repo_name,username):
-
     email= ""
-
     commit_data = requests.get("https://github.com/{}/{}/commits?author={}".format(username,repo_name,username)).text
     soup = BeautifulSoup(commit_data,"lxml")
-
     a_tags = soup.findAll("a")
     for a_tag in a_tags:
         URL = a_tag.get("href")
-
         if URL.startswith("/{}/{}/commit/".format(username,repo_name)):
             label = str(a_tag.get("aria-label"))
             if "Merge" not in label and label != "None":
@@ -74,34 +58,33 @@ def get_latest_commit(repo_name,username):
                 except ValueError:
                     return "Not enough information."
                 break
-
     if email != "":
         return email
     else:
         return "Not enough information."
 
 if __name__ == '__main__':
-
     Logo.header()         # For Displaying Logo
-
     parser = argparse.ArgumentParser()
     parser.add_argument('repositoryURL',help = " Path to repository.")
-
+    try:
+        import requests
+        from bs4 import BeautifulSoup
+    except ImportError:
+        colors.error('Error importing requests module.')
+        sys.exit(1)
     if len(sys.argv)>1:
         args = parser.parse_args()
         repository_link = args.repositoryURL
     else:
         repository_link = input("\033[37mEnter the repository address :: \x1b[0m")    # Getting repository Address
         print("\n")
-
     repository_link = format_url(repository_link)       # Assuring that URL starts with https://
-
     try:
         html = requests.get(repository_link,timeout=8).text       # Getting HTML page of repository
     except requests.exceptions.MissingSchema as e:
         colors.error("Enter the repositories url in given format [ https://github.com/username/repository_name ]")
         sys.exit(1)
-
     result=verify_url(html)                             # Checking if the url given is of a repository or not.
     if result is True:
         colors.success("Got the repository data ")
@@ -109,18 +92,19 @@ if __name__ == '__main__':
     else:
         colors.error("Please enter the correct URL ")
         sys.exit(0)
-
     soup1=BeautifulSoup(html,"lxml")                    # Parsing the html data using BeautifulSoup
-
+    try:
+        import data
+    except ImportError:
+        colors.error('Error importing data module')
+        sys.exit(1)
     title=getting_header(soup1)                         # Getting the title of the page
     data.header=title                                   # Storing title of the page as Project Title
     colors.success("Repository Title : "+title)
     time.sleep(1)
-
     star_value=0
     watch_value = 0
     fork_value =0
-
     a_tags=soup1.findAll("a")                           # Finding all the 'a' tags in response html data.
     for a_tag in a_tags:                                # Finding total stargazers of the repository
         string=a_tag.get("href")
@@ -143,10 +127,8 @@ if __name__ == '__main__':
             time.sleep(1)
             fork_value=int(fork_value)
             break
-
     stargazer_link=repository_link+"/stargazers"
     colors.process("Fetching stargazers list")
-
     while (stargazer_link!=None):
         stargazer_html=requests.get(stargazer_link).text
         soup2=BeautifulSoup(stargazer_html,"lxml")
@@ -163,34 +145,27 @@ if __name__ == '__main__':
             data.name_list.append(a_tag[0].get_text())
             username=a_tag[0].get("href")
             data.username_list.append(username[1:])
-
     count=1
     pos=0
     colors.process("Doxing started ...\n")
     time.sleep(1)
     print(colors.red+"--------------------------------------------------------------------------",colors.green,end="\n\n")
     while(count<=star_value):
-
         starer_url="https://github.com/"+data.username_list[pos]
         user_html=requests.get(starer_url).text
         soup3=BeautifulSoup(user_html,"lxml")
-
         repo_data = requests.get("https://github.com/{}?tab=repositories&type=source".format(data.username_list[pos])).text
         repo_soup = BeautifulSoup(repo_data,"lxml")
-
         a_tags = repo_soup.findAll("a")
         repositories_list = []
         for a_tag in a_tags:
             if a_tag.get("itemprop") == "name codeRepository":
                 repositories_list.append(a_tag.get_text().strip())
-
         if len(repositories_list) > 0:
-
             email = get_latest_commit(repositories_list[0],data.username_list[pos])
             data.email_list.append(str(email))
         else:
             data.email_list.append("Not enough information.")
-
         if(user_html!=None):
             items=soup3.findAll("a",{"class":"UnderlineNav-item"})
             for item in items[1:]:
@@ -210,8 +185,12 @@ if __name__ == '__main__':
                     a_tag=item.findAll("span")
                     following_count=a_tag[0].get_text()
                     data.following_list.append(formated(following_count))
-            structer.plotdata(star_value,pos,count)
-
+            try:
+                import structer
+                structer.plotdata(star_value,pos,count)
+            except ImportError:
+                colors.error("Error importing structer module.")
+                sys.exit(1)
         count+=1
         pos+=1
     print(colors.green+"\n--------------------------------------------------------------------------",end="\n\n")
