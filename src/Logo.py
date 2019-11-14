b6c4c54212cfe63b5fe7e784dadc4f403cdde01a
Prateek Mishra <pr4t333k@gmail.com>
2019-11-15 03:00:10 +0530

    new logo for stardox [2/2]

--- src/Logo.py
+++ src/Logo.py
@@ -1,13 +1,3 @@
+import colors
 def header():
-    print('''
-\033[93m           ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-\033[93m          sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-\033[93m         sssssss        \033[31m    ssssssssss      ss     ss       sss     ss      sss    ss       ss      ss   sss    sss
-\033[93m        sssssss         \033[31m      ssss        sss     sss      sss      ss     sss      ss     ss      ss     ss   ss
-\033[93m       ssssssssssssss   \033[31m     ssss        sss     sss      sss      ss     sss       ss    ss      ss      ss  ss
-\033[93m       ssssssssssssss   \033[31m    ssss        sssssssssss      sssssssssss     sss       ss    ss      ss       ssss
-\033[93m                ssss    \033[31m   ssss        sssssssssss      sssssss         sss       ss    ss      ss       ssss
-\033[93m               ssss     \033[31m  ssss        sss     sss      sss  sss        sss       ss    ss      ss       ss  ss
-\033[93m    ssssssssssssss      \033[31m ssss        sss     sss      sss   sss       sss     ss     ssssssssss        ss   ss
-\033[93m  sssssssssssssss       \033[31mssss        sss     sss      sss     sss     sssssssss      ssssssssss       sss    sss       \033[137m Made By :\033[93m Pr0t0n
-    ''')
+    print('\n\t\t{}☰{} 𝕊 𝕋 𝔸 ℝ{} 𝔻 𝕆 𝕏 {}☰{}\n'.format(colors.yellow,colors.yellow,colors.red,colors.yellow,colors.green))
