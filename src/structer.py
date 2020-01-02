8ec1ac46674849df49e4c72f519b10628b9d6309
CaineArdayfio <ardayfiocaine@gmail.com>
2020-01-01 23:47:17 -0500

    This commit denotes sequential, modular, and partially complete data gathering. To complete the modular and sequential development, it is necessary to update the save() function and the data.py and structer.py files

--- src/structer.py
+++ src/structer.py
@@ -1,13 +1,9 @@
 import data
 
 
-def plotdata(stars, pos, count):
-
-    if count == 1:
-        print("\033[34m    {}   ".format(data.header))
-        print("\033[1;37m¯¯¯¯{}¯¯¯¯".format('¯' * len(data.header)), end="\n")
-        print(
-            '''\033[1;37m{}
+def plotdata(stars):
+    print(
+        '''\033[1;37m{}
 \033[1;37m|-----\033[1;32m{} (@{})
 \033[1;37m|  |
 \033[1;37m|  |--\033[1;32mTotal Repsitories :: \033[1;37m{}
@@ -15,42 +11,9 @@ def plotdata(stars, pos, count):
 \033[1;37m|  |--\033[1;32mTotal Followers   :: \033[1;37m{}
 \033[1;37m|  |--\033[1;32mTotal Following   :: \033[1;37m{}
 \033[1;37m|  |--\033[1;32mUser Email        :: \033[1;37m{}
-\033[1;37m|'''.format("|", data.name_list[pos], data.username_list[pos],
-                      data.repo_list[pos].strip(),
-                      data.star_list[pos].strip(),
-                      data.followers_list[pos].strip(),
-                      data.following_list[pos].strip(),
-                      data.email_list[pos].strip()))
-
-    elif(count > 1 and count < stars):
-        print(
-            '''\033[1;37m{}
-\033[1;37m|-----\033[1;32m{} (@{})
-\033[1;37m|  |
-\033[1;37m|  |--\033[1;32mTotal Repsitories :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mTotal Stars       :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mTotal Followers   :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mTotal Following   :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mUser Email        :: \033[1;37m{}
-\033[1;37m|'''.format("|", data.name_list[pos], data.username_list[pos],
-                      data.repo_list[pos].strip(),
-                      data.star_list[pos].strip(),
-                      data.followers_list[pos].strip(),
-                      data.following_list[pos].strip(),
-                      data.email_list[pos].strip()))
-
-    elif count == stars:
-        print('''{}
-\033[1;37m|-----\033[1;32m{} (@{})
-\033[1;37m|  |
-\033[1;37m|  |--\033[1;32mTotal Repsitories :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mTotal Stars       :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mTotal Followers   :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mTotal Following   :: \033[1;37m{}
-\033[1;37m|  |--\033[1;32mUser Email        :: \033[1;37m{}
-'''.format("|",
-           data.name_list[pos], data.username_list[pos],
-           data.repo_list[pos].strip(), data.star_list[pos].strip(),
-           data.followers_list[pos].strip(),
-           data.following_list[pos].strip(),
-           data.email_list[pos].strip()))
+\033[1;37m|'''.format("|", data.info_dict["name"], data.info_dict["username"],
+                    data.info_dict["repos"].strip(),
+                    data.info_dict["stars"].strip(),
+                    data.info_dict["followers"].strip(),
+                    data.info_dict["following"].strip(),
+                    data.info_dict["email"].strip()))
