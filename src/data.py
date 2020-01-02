4208b3bee97a647950e166eab5a00dcfa88d333e
CaineArdayfio <ardayfiocaine@gmail.com>
2020-01-02 01:28:33 -0500

    Temporary Full Functioning Code, must test before full deployment

--- src/data.py
+++ src/data.py
@@ -1,33 +1,19 @@
+# For Storing the Repository name
+header = ""
+# For Storing Usernames of stargazers
+username_list = []
+# For Storing Names of the stargazers
+name_list = []
+
+# For storing the individual user data
 info_dict = {    
     "stars": None,
     "repos": None,
     "followers": None,
     "email": None,
     "following": None,
-    "name": None
+    "name": None,
+    "username": None
 }
-"""
-# For Storing name of the stargazers
-name_list = []
 
-# For Storing Total stars by the stargazers
-star_list = []
-
-# For Storing Total number of repositories by stargazers
-repo_list = []
-
-# For Storing Total number of followers of stargazers
-followers_list = []
-
-# For Storing emails of all the stargazers
-email_list = []
-
-# For Storing Total number of following of stargazers
-following_list = []
-"""
-# For Storing the Repository name
-header = ""
-# For Storing Username of stargazers
-username_list = []
-# For Storing name of the stargazers
-name_list = []
+info_dicts = list()
